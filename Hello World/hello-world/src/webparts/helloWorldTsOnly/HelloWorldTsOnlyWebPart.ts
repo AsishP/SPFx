ea5dd9231227cc06538a6768f86db6c864d518da
Asish Padhy <asish2k14@outlook.com>
2018-05-02 20:09:18 +1000

    Dev 2 commit 1

--- Hello World/hello-world/src/webparts/helloWorldTsOnly/HelloWorldTsOnlyWebPart.ts
+++ Hello World/hello-world/src/webparts/helloWorldTsOnly/HelloWorldTsOnlyWebPart.ts
@@ -19,7 +19,7 @@ export default class HelloWorldTsOnlyWebPart extends BaseClientSideWebPart<IHell
           <div class="ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}">
             <div class="ms-Grid-col ms-u-lg10 ms-u-xl8 ms-u-xlPush2 ms-u-lgPush1">
               <span class="ms-font-xl ms-fontColor-white">Welcome to SharePoint!</span>
-              <p class="ms-font-l ms-fontColor-white">Customize SharePoint experiences using Web Parts at master 5.</p>
+              <p class="ms-font-l ms-fontColor-white">Customize SharePoint experiences using Web Parts 1</p>
               <p class="ms-font-l ms-fontColor-white">${escape(this.properties.description)}</p>
               <a href="https://aka.ms/spfx" class="${styles.button}">
                 <span class="${styles.label}">Learn more</span>
